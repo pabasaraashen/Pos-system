6a326cedae1a0e0908d63eb22c832dd2fa46d620
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-07-02 11:46:09 +0530

    Update Bottomnavbar.jsx, Header.jsx

--- pos-frontend/src/components/shared/Header.jsx
+++ pos-frontend/src/components/shared/Header.jsx
@@ -16,11 +16,26 @@ const Header = () => {
 
      {/*search bar*/}
      <div className="flex items-center gap-4 bg-[#1a1a1a] rounded-[20px] px-5 py-2 w-[500px]">
+        <FaSearch className="text-[#f5f5f5]" />
         <input
          type="text"
          placeholder="Search"
          className="bg-[#1f1f1f] outline-none text-[#f5f5f5]"
         />
+     </div>
+
+     {/*login profile*/}
+     <div className="flex items-center gap-4">
+        <div className="bg-[#1f1f1f] rounded-[15px] p-3 cursor-pointer">
+            <FaBell className="text-[#f5f5f5] text-2xl"/>
+        </div>
+        <div className="flex items-center gap-3 cursor-pointer">
+            <FaUserCircle className="text-[#f5f5f5] text-4xl"/>
+        <div className="flex flex-col items-start">
+            <h1 className="text-md text-[#f5f5f5]">Pabasara</h1>
+            <p className="text-xs text-[#7c7777]">Admin</p>
+        </div>
+        </div>
      </div>
     </header>
   )
--- pos-frontend/src/pages/Home.jsx
+++ pos-frontend/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Bottomnavbar from '../components/Bottomnavbar'
 
 const Home = () => {
   return (
-    <div>kkkkkkkkkkkkkkkkkkkkkk</div>
+    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
+       {/*left*/}
+       <div className='flex-[3] bg-amber-300'></div>
+       {/*right*/}
+       <div className='flex-[2] bg-blue-400'></div>
+       <Bottomnavbar/>
+    </section>
   )
 }
 
