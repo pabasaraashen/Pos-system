ff2eeb43430c129873f689345395adbc70d5ffdd
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-07-07 20:51:05 +0530

    Update Menu.jsx on cart section

--- pos-frontend/src/pages/Menu.jsx
+++ pos-frontend/src/pages/Menu.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import BackButton from '../components/shared/BackButton';
 import MenuContainer from '../components/menu/MenuContainer';
+import { RiDashboard2Fill } from 'react-icons/ri';
+import { FaNotesMedical } from 'react-icons/fa';
 
 const Menu = () => {
   return (
@@ -41,9 +43,20 @@ const Menu = () => {
          <div className='px-4 py-2'>
             <h1 className='text-lg text-[#e4e4e4] font-semibold'>Order Details</h1>
             <div className='mt-4 overflow-y-scroll scroll-hide h-[380px]'>
+              <div className='bg-[#1f1f1f] rounded-lg px-4 py-4 mb-2'>
+                <div className='flex items-center justify-between'>
                 <h1 className='text-[#ababab] font-semibold text-md'>
                     Chicken Tikka
                 </h1>
+                <p className='text-[#ababab] font-semibold'>x2</p>
+                </div>
+                <div className='flex items-center justify-between mt-3'>
+                  <div className='flex items-center gpa-3'>
+                    <RiDashboard2Fill className='text-[#ababab] cursor-pointer size={20}'/>
+                    <FaNotesMedical className='text-[#ababab] cursor-pointer size={20}'/>
+                  </div>  
+                </div>
+              </div>
             </div>
          </div>
          {/*bills*/} 
