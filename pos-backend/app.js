95a1f54240fb992c572889444c6f4e1f4e69fa42
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-07-15 22:36:25 +0530

    create error handler

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -1,15 +1,29 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
+//const { config } = require("dotenv");
+const config = require("./config/config");
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
+const createHttpError = require("http-errors");
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = config.port;
 connectDB();
 
+// Root Endpoint
 app.get("/", (req,res) => {
+
+    const err = createHttpError(404, "something gone wrong!");
+    throw err;
+
+
     res.json({message : "Hello from POS Server!"});
 })
 
+//Globval Error Handler
+app.use(globalErrorHandler);
+
+// Server
 app.listen(PORT, () => {
     console.log(`POS Server is listening on port ${PORT}`);
 })
