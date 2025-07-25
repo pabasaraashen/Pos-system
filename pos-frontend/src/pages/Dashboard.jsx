0ea2fd54336edc9303b64d8e768b698f8f8fa0f6
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-07-25 14:17:11 +0530

    Add action buttons to Dashboard page

--- pos-frontend/src/pages/Dashboard.jsx
+++ pos-frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { MdTableBar, MdCategory } from "react-icons/md";
+import { BiSolidDish } from "react-icons/bi";
+
+const buttons = [
+  { label: "Add Table", icon: <MdTableBar />, action: "table" },
+  { label: "Add Category", icon: <MdCategory />, action: "category" },
+  { label: "Add Dishes", icon: <BiSolidDish />, action: "dishes" },
+];
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='bg-[#1f1f1f] h-[calc(100vh-5rem)]'>
+        <div className='container mx-autoflex items-center justify-between py-14 px-6 md:px-4'>
+            <div className='flex items-center gap-3'>
+                {
+                    buttons.map(({ label, icon, action }) => {
+                       return(
+                         <button className='bg-black hover:bg-gray-800 text-white py-3 px-8 rounded-lg font-semibold text-md flex items-center gap-2'>
+                            {icon}
+                            {label}
+                        </button>
+                       )
+
+                    })
+
+                }
+            </div>
+
+        </div>
+        
+    </div>
   )
 }
 
