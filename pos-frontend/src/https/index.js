f9aebc72a876ff9af93cbb30319ee48994cf2358
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-07-28 14:52:29 +0530

    Update index.js

--- pos-frontend/src/https/index.js
+++ pos-frontend/src/https/index.js
@@ -11,8 +11,11 @@ const api = axios.create({
 
 export default api;
 
-//API Endpoints
+// Auth API Endpoints
 export const login = (data) => api.post('/api/user/login', data);
 export const register = (data) => api.post('/api/user/register', data);
 export const getUserData = () => api.get('/api/user');
 export const logout = () => api.post('/api/user/logout');
+
+// Order API Endpoints
+export const getOrders = () => api.get('/api/order');
